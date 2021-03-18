705a2b12331dea5245af488dabb989225875a7b9
Vulcan <vulcan.lee@gmail.com>
2021-03-18 18:56:59 +0800

    .NET Core 使用地址來查詢 GPS 經度緯度

---
+++ csAddressToGPS/csAddressToGPS/Program.cs
@@ -0,0 +1,20 @@
+﻿using Geocoding;
+using Geocoding.Google;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csAddressToGPS
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IGeocoder geocoder = new GoogleGeocoder() { ApiKey = "this-is-my-optional-google-api-key" };
+            IEnumerable<Address> addresses = await geocoder.GeocodeAsync("高雄市鼓山區明倫路59號");
+            Console.WriteLine("Formatted: " + addresses.First().FormattedAddress);
+            Console.WriteLine("Coordinates: " + addresses.First().Coordinates.Latitude + ", " + addresses.First().Coordinates.Longitude); 
+        }
+    }
+}
