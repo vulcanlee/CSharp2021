97aa5a5efd566ebbf578f49204d1f54e232eed92
Vulcan <vulcan.lee@gmail.com>
2021-03-19 22:20:53 +0800

    使用 Blazor 專案在 JavaScript 內呼叫 C# 執行個體方法

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 |專案名稱|說明|部落格網址|
 |-|-|-|
 |csAddressToGPS|.NET Core 使用地址來查詢 GPS 經度緯度||
-||||
+|bzCallCsharpInstanceMethodFromJavaScript|使用 Blazor 專案在 JavaScript 內呼叫 C# 執行個體方法||
 ||||
 ||||
 ||||
---
+++ bzCallCsharpInstanceMethodFromJavaScript/bzCallCsharpInstanceMethodFromJavaScript/wwwroot/js/JavaScript.js
@@ -0,0 +1,10 @@
+﻿var DotNetObject = {}
+function showPrompt(text) {
+    var result = prompt(text, 'Type your name here');
+    //呼叫該 .NET 執行個體(由類別 BindDotNetInstance 所產生)的 SayHello 方法
+    DotNetObject.invokeMethodAsync('SayHello', result);
+}
+function SetDotNetObjectJS(dotnetHelper) {
+    //將該 .NET 執行個體設定成為 JavaScript 的全域變數
+    DotNetObject = dotnetHelper;
+}
