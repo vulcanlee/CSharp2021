9f6fcb9bc6ee318aa69e300634c2ec80c8c51ee4
Vulcan <vulcan.lee@gmail.com>
2021-03-27 23:00:30 +0800

    Blazor 專案使用ASP.NET Core 中的選項模式取得巢狀設定宣告內容

---
+++ bzOptionsNest/bzOptions/中文選項1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bzOptions
+{
+    public class 中文選項1
+    {
+        public string Child1 { get; set; }
+        public string Child2 { get; set; }
+        public string 節點中文3 { get; set; }
+        public Nestnode1 NestNode1 { get; set; }
+    }
+
+    public class Nestnode1
+    {
+        public int SubNode1 { get; set; }
+        public string SubNode2 { get; set; }
+        public string SubNode3 { get; set; }
+    }
+
+}
